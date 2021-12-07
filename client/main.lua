491a3239c06f8e98bfbd2ee2dee30c9768f4153f
KYLE S <79766794+418Gaming@users.noreply.github.com>
2021-12-07 11:03:31 -0500

    UFRP - Updated keys used

--- client/main.lua
+++ client/main.lua
@@ -3,10 +3,7 @@ local keyPressed = false
 local inKeyBinding = false
 local availableKeys = {
     {289, "F2"},
-    {170, "F3"},
-    {166, "F5"},
     {167, "F6"},
-    {168, "F7"},
     {56, "F9"},
     {57, "F10"},
 }
